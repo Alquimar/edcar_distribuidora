63be6e3755b493de2e7e37f1543c888ece58ecc1
Alquimar Sousa Almeida Filho <alquimars@gmail.com>
2020-07-08 10:10:52 -0300

    Falta página de contato

--- edcar_distribuidora/core/urls.py
+++ edcar_distribuidora/core/urls.py
@@ -4,6 +4,8 @@ from edcar_distribuidora.core import views
 
 app_name = "core"
 urlpatterns = [
-    path("", views.getTodos, name="home"),
-    path("categoria/<int:categoria_id>", views.getItensCategoria, name="itens_categoria"),
+    path("", views.home, name="home"),
+    path("quem-somos/", views.quem_somos, name="quem_somos"),
+    path("contato/", views.contato, name="contato"),
+    path("categoria/<int:categoria_id>", views.itens_categoria, name="itens_categoria"),
 ]
--- edcar_distribuidora/core/views.py
+++ edcar_distribuidora/core/views.py
@@ -3,7 +3,19 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Departamento, Categoria, Item
 
-def getTodos(request):
+def quem_somos(request):
+    template_name = "core/quem_somos.html"
+    context = {}
+    return render(request, template_name, context)
+
+
+def contato(request):
+    template_name = "core/contato.html"
+    context = {}
+    return render(request, template_name, context)
+
+
+def home(request):
     departamentos = Departamento.objects.filter(ativo=True)
     produtos = Item.objects.filter(ativo=True)
     template_name = "core/home.html"
@@ -14,7 +26,7 @@ def getTodos(request):
     return render(request, template_name, context)
 
 
-def getItensCategoria(request, categoria_id):
+def itens_categoria(request, categoria_id):
     departamentos = Departamento.objects.filter(categorias__isnull=False, ativo=True).distinct()
     produtos = Item.objects.filter(categoria_id=categoria_id)
     template_name = "core/itens_categoria.html"
